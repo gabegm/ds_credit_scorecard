305cb4250558597e032ac47d76070ba74b8c6267
Gabriel Gauci Maistre <gaucimg@MT-MBP15GauciMG.local>
2019-06-14 13:42:13 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ds_credit_scorecard',
+    packages=find_packages(),
+    version='0.1.0',
+    description='mathematical model attempting to provide a quantitative estimate of the probability that a customer will default on a loan.',
+    author='gabegm',
+    license='MIT',
+)
